778f96ad4dcee988eee229724cc28b6e9d8cad6d
evgeniy-tropin <evgeniy.tropin@flexaspect.com>
2023-05-20 19:30:51 +0300

    Improvements for logo, footer and file upload

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -27,7 +27,6 @@
                     <button type="submit" class="btn btn-primary">Save</button>
                 </form>
             </div>
-            <div class="card-footer align-items-center d-flex justify-content-between"></div>
         </div>
     </div>
 
@@ -46,17 +45,50 @@
                         </ul>
                     </div>
                 @endif
-                <form id="file-form" action="{{ route('web::file::upload') }}" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate="">
+                <div class="row">
+                    <div class="col-xl-4 col-md-6 mb-3 mb-lg-4">
+                        <div class="card overflow-hidden">
+                            <div class="card-body p-3 bg-primary border-primary text-white rounded d-flex align-items-center" style="cursor: pointer;">
+                                <div class="flex-shrink-0 position-relative bg-white text-primary rounded-circle mr-3 d-flex align-items-center justify-content-center">
+                                    <svg class="bd-placeholder-img rounded-circle" width="60" height="60" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 60x60" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        <rect width="100%" height="100%" fill="#fff"></rect>
+                                    </svg>
+                                    <i class="position-absolute h2 bi bi-file-earmark-arrow-up"></i>
+                                </div>
+                                <div class="flex-grow-1 text-start">
+                                    <h5 class="h3 d-block fw-bold mb-1">Files</h5>
+                                    <h6 class="mb-0 fw-normal">Upload files to Library</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6 mb-3 mb-lg-4">
+                        <div class="card overflow-hidden">
+                            <div class="card-body p-3 rounded d-flex align-items-center" style="cursor: pointer;">
+                                <div class="flex-shrink-0 position-relative bg-white text-primary rounded-circle mr-3 d-flex align-items-center justify-content-center">
+                                    <svg class="bd-placeholder-img rounded-circle" width="60" height="60" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 60x60" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        <rect width="100%" height="100%" fill="#f6f5fe"></rect>
+                                    </svg>
+                                    <i class="position-absolute h3 bi bi-search"></i>
+                                </div>
+                                <div class="flex-grow-1 text-start">
+                                    <h5 class="h3 d-block fw-bold mb-1">Links Crawler</h5>
+                                    <h6 class="mb-0 fw-normal">Get content from web links</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <form id="file-form" class="dropzone" action="{{ route('web::file::upload') }}" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate="">
                     @csrf
                     <input type="hidden" name="library_id" value="{{ $library->id ?? '' }}">
-                    <div class="form-group">
-                        <label for="file" class="form-label">Choose file</label>
-                        <input class="form-control" type="file" id="file" name="file">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Upload</button>
+{{--                    <div class="form-group">--}}
+{{--                        <label for="file" class="form-label">Choose file</label>--}}
+{{--                        <input class="form-control" type="file" id="file" name="file">--}}
+{{--                    </div>--}}
+{{--                    <button type="submit" class="btn btn-primary">Upload</button>--}}
                 </form>
             </div>
-            <div class="card-footer align-items-center d-flex justify-content-between"></div>
         </div>
     </div>
 
@@ -102,8 +134,22 @@
                     </table>
                 </div>
             </div>
-            <div class="card-footer align-items-center d-flex justify-content-between"></div>
         </div>
     </div>
     @endif
+@endsection
+
+@section('js')
+    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+    <script>
+        Dropzone.autoDiscover = false;
+        $(document).ready(function () {
+            $("#file-form").dropzone({
+                url: '{{ route('web::file::upload') }}',
+                maxFiles: 1,
+                acceptedFiles: '.txt,.csv,.doc,.docx,.abw,.djvu,.docm,.dot,.dotx,.html,.hwp,.lwp,.md,.odt,.pages,.pdf,.rst,.rtf,.tex,.wpd,.wps,.zabw,.azw,.azw3,.azw4,.cbc,.cbr,.cbz,.chm,.epub,.fb2,.htm,.htmlz,.lit,.lrf,.mobi,.pdb,.pml,.prc,.rb,.snb,.tcr,.txtz,.dps,.key,.odp,.pot,.potx,.pps,.ppsx,.ppt,.pptm,.pptx,.et,.numbers,.ods,.xlr,.xls,.xlsm,.xlsx',
+                maxFilesize: 3
+            });
+        });
+    </script>
 @endsection
