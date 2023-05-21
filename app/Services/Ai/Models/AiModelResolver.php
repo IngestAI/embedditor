a0331362a049a847601c36d11056ec939cc7f48c
Igor <igor.shaev@gmail.com>
2023-05-21 15:50:31 +0200

    Playground models.

--- app/Services/Ai/Models/AiModelResolver.php
+++ app/Services/Ai/Models/AiModelResolver.php
@@ -21,25 +21,25 @@ final class AiModelResolver
         return new static($slug);
     }
 
-    public function resolve(string $query): ConverterHandler
+    public function resolve(string $query): AiModel
     {
         switch ($this->slug) {
             case 'gpt-3.5-turbo':
-                return new Gpt35TurboAiModel($this->query);
+                return new Gpt35TurboAiModel($query);
             case 'gpt-4':
-                return new Gpt4AiModel($this->query);
+                return new Gpt4AiModel($query);
             case 'text-davinci-003':
-                return new TextDavinci003AiModel($this->query);
+                return new TextDavinci003AiModel($query);
             case 'text-davinci-002':
-                return new TextDavinci002AiModel($this->query);
+                return new TextDavinci002AiModel($query);
             case 'text-curie-001':
-                return new TextCuris001AiModel($this->query);
+                return new TextCuris001AiModel($query);
             case 'text-babbage-001':
-                return new TextBabbage001AiModel($this->query);
+                return new TextBabbage001AiModel($query);
             case 'text-ada-001':
-                return new TextAda001AiModel($this->query);
+                return new TextAda001AiModel($query);
         }
 
-        return new NullAiModel($this->rawFile);
+        return new NullAiModel();
     }
 }
